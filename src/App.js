7525214c39faf1a481f4f4e4e69c8138f656906a
santoshkalyankumar-maker <santoshkalyankumar@gmail.com>
2024-12-13 15:46:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className='App'>
+    <h1>Employee Information</h1>
+    <h1>Name: Santosh</h1>
+    <h3> Moble : 8978027512</h3>
+    <h3>Address: Hyderbad</h3>
+  </div>
+  );
+}
+
+export default App;
